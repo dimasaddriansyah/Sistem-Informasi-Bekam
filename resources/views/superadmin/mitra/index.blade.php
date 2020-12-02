eea9d6043727b2042187c325ea9d4ccd591cc96f
dimasaddriansyah <dimasaddriansyah14@gmail.com>
2020-12-02 15:43:18 +0700

    API for Pelanggan

--- resources/views/superadmin/mitra/index.blade.php
+++ resources/views/superadmin/mitra/index.blade.php
@@ -165,7 +165,7 @@
                 <div class="col-12 mt-3">
                     <div class="card">
                         <div class="card-body">
-                          <table id="example1" class="table table-bordered table-striped table-responsive">
+                          <table id="example1" class="table table-bordered table-striped table-responsive-lg">
                                 <thead>
                                     <tr>
                                         <th>No</th>
